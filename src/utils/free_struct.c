f616392ae642bd881fa0a7a46abb953b0ef1c964
Rafael Drouart <rafael.drouart@epitech.eu>
2024-04-24 20:27:10 +0200

    [~] Correcting the free_struct function (removing the valgrind error but miss some free)

--- src/utils/free_struct.c
+++ src/utils/free_struct.c
@@ -12,15 +12,8 @@
 void free_corewar(corewar_t *corewar)
 {
     free(corewar->vm->mem);
-    for (int i = 0; i < corewar->vm->nb_champs; i++) {
-        free(corewar->vm->champs[i]->name);
-        free(corewar->vm->champs[i]->comment);
-        free(corewar->vm->champs[i]->prog);
-        free(corewar->vm->champs[i]);
-    }
+    free(corewar->fd);
     free(corewar->vm->champs);
     free(corewar->vm);
-    close(corewar->fd1);
-    close(corewar->fd2);
     free(corewar);
 }
