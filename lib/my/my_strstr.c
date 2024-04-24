8e6466d89890644eb07494513b9618b06054b81a
Rafael Drouart <rafael.drouart@epitech.eu>
2024-04-24 20:25:36 +0200

    [-] Removing coding style error in my_strstr function and separates it into two functions

--- lib/my/my_strstr.c
+++ lib/my/my_strstr.c
@@ -5,18 +5,25 @@
 ** Day 06 Task 05
 */
 
+char *tmp(char *str, char const *to_find, int i, int j)
+{
+    if (str[i] == to_find[j]) {
+        j++;
+        if (to_find[j] == '\0')
+            return &str[i - j + 1];
+    } else
+        j = 0;
+    return 0;
+}
+
 char *my_strstr(char *str, char const *to_find)
 {
     int i = 0;
     int j = 0;
 
     while (str[i] != '\0') {
-        if (str[i] == to_find[j]) {
-            j++;
-            if (to_find[j] == '\0')
-                return &str[i - j + 1];
-        } else
-            j = 0;
+        if (tmp(str, to_find, i, j) != 0)
+            return tmp(str, to_find, i, j);
         i++;
     }
     return 0;
