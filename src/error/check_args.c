a21ace2242bbd0ea2cbb7fe2162ef668b196f9d6
Rafael Drouart <rafael.drouart@epitech.eu>
2024-04-24 10:29:41 +0200

    [~] Finishing the check_args function if there are any other parameters to open the file

--- src/error/check_args.c
+++ src/error/check_args.c
@@ -10,11 +10,21 @@
 #include "my.h"
 #include "corewar.h"
 
+void open_file(char **av, corewar_t *corewar, int i)
+{
+    if (corewar->fd1 < 0)
+        corewar->fd1 = open(av[i], O_RDONLY);
+    else if (corewar->fd2 < 0)
+        corewar->fd2 = open(av[i], O_RDONLY);
+}
+
 int check_args(char **av, corewar_t *corewar)
 {
-    corewar->fd1 = open(av[1], O_RDONLY);
-    corewar->fd2 = open(av[2], O_RDONLY);
-    if (corewar->fd1 == -1 || corewar->fd2 == -1)
+    corewar->fd1 = -1;
+    corewar->fd2 = -1;
+    for (int i = 1; av[i]; i++)
+        open_file(av, corewar, i);
+    if (corewar->fd1 <= 0 || corewar->fd2 <= 0)
         return 84;
     return 0;
 }
