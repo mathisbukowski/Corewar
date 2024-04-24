13a3d2365bd4c10b3445592b8e7fdae99345d2a2
Rafael Drouart <rafael.drouart@epitech.eu>
2024-04-24 20:16:42 +0200

    [+] Adding the my_strstr function to check if the args are file finishing by .cor to get the champions

--- lib/my/my_strstr.c
+++ lib/my/my_strstr.c
@@ -7,5 +7,17 @@
 
 char *my_strstr(char *str, char const *to_find)
 {
+    int i = 0;
+    int j = 0;
+
+    while (str[i] != '\0') {
+        if (str[i] == to_find[j]) {
+            j++;
+            if (to_find[j] == '\0')
+                return &str[i - j + 1];
+        } else
+            j = 0;
+        i++;
+    }
     return 0;
 }
