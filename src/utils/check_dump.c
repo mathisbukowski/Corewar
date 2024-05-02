b732ec74122ceba1405160369f87ac918dc34b0b
Rafael Drouart <rafael.drouart@epitech.eu>
2024-05-02 10:43:40 +0200

    [~] Modifying the check_dump function to check all the argv

--- src/utils/check_dump.c
+++ src/utils/check_dump.c
@@ -8,14 +8,23 @@
 #include "corewar.h"
 #include "my.h"
 
-int check_dump(corewar_t *corewar, char **av)
+int get_current_arg(char **av, int i, corewar_t *corewar)
 {
-    if (my_strcmp(av[1], "-dump") == 0) {
-        if (my_str_isnum(av[2]) == 0) {
-            corewar->vm->dump = my_getnbr(av[2]);
+    if (my_strcmp(av[i], "-dump") == 0) {
+        if (my_str_isnum(av[i + 1]) == 0) {
+            corewar->vm->dump = my_getnbr(av[i + 1]);
             return 0;
         }
         return 84;
+    }
+    return 0;
+}
+
+int check_dump(corewar_t *corewar, char **av)
+{
+    for (int i = 0; av[i]; i++) {
+        if (get_current_arg(av, i, corewar) == 84)
+            return 84;
     }
     return 0;
 }
