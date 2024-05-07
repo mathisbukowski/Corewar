faa4621adf8ef04611eacced8405591f49946a14
Rafael Drouart <rafael.drouart@epitech.eu>
2024-05-07 15:42:28 +0200

    [+] Adding the aff instruction function

---
+++ src/instructions/aff_inst.c
@@ -0,0 +1,19 @@
+/*
+** EPITECH PROJECT, 2024
+** corewar
+** File description:
+** aff_inst.c
+*/
+
+#include "corewar.h"
+
+int aff_command(instruction_t *instr, champion_t *champ, corewar_t *corewar)
+{
+    int reg = instr->args[0];
+    int value = champ->reg[reg - 1];
+
+    if (!instr || !champ || !corewar)
+        return -1;
+    my_putchar(value % 256);
+    return 0;
+}
