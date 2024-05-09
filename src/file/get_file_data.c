f2893c41aab937b668b364179eb9c5298964ade1
Rafael Drouart <rafael.drouart@epitech.eu>
2024-05-09 11:35:18 +0200

    [~] Trying to fix the function that get the arguments

--- src/file/get_file_data.c
+++ src/file/get_file_data.c
@@ -27,7 +27,7 @@ int swap_endian(int value)
     right_middle_byte <<= 8;
     rightmost_byte <<= 0;
     result = (leftmost_byte | left_middle_byte
-              | right_middle_byte | rightmost_byte);
+        | right_middle_byte | rightmost_byte);
     return result;
 }
 
@@ -46,35 +46,42 @@ int open_file(char *file, header_t *header)
     }
     header->prog_size = swap_endian(header->prog_size);
     header->magic = swap_endian(header->magic);
+    close(fd);
     return 0;
 }
 
-int get_instructions(char *file, corewar_t *corewar, int i)
+int get_instructions(char *file, corewar_t *corewar)
 {
     int fd = open(file, O_RDONLY);
     int size = sizeof(header_t);
-    int prog_size = corewar->champs[i].prog_size;
-    unsigned char *prog = malloc(prog_size);
+    int prog_size = corewar->champs->prog_size;
+    int i = 0;
 
     if (fd == -1)
         return 84;
     lseek(fd, size, SEEK_SET);
-    if (read(fd, prog, prog_size) == -1)
+    corewar->champs->instructs = malloc(sizeof(instruction_t) * prog_size);
+    if (corewar->champs->instructs == NULL)
         return 84;
-    corewar->champs[i].prog = prog;
+    for (i = 0; i < prog_size; i++) {
+        read(fd, &corewar->champs->instructs[i].opcode, 1);
+        if (corewar->champs->instructs[i].opcode < 1
+            || corewar->champs->instructs[i].opcode > 16)
+            return 84;
+    }
     return 0;
 }
 
-int get_file_data(char *file, corewar_t *corewar, int i)
+int get_file_data(char *file, corewar_t *corewar)
 {
     header_t header;
 
     if (open_file(file, &header) == 84)
         return 84;
-    corewar->champs[i].name = my_strdup(header.prog_name);
-    corewar->champs[i].comment = my_strdup(header.comment);
-    corewar->champs[i].prog_size = header.prog_size;
-    if (get_instructions(file, corewar, i) == 84)
+    corewar->champs->name = my_strdup(header.prog_name);
+    corewar->champs->comment = my_strdup(header.comment);
+    corewar->champs->prog_size = header.prog_size;
+    if (get_instructions(file, corewar) == 84)
         return 84;
     return 0;
 }
