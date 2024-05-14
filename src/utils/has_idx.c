c5639c8376fba518cdaae62b966e8d1dd627e2f6
Mathis Bukowski <mathis.bukowski@epitech.eu>
2024-05-14 22:25:27 +0200

    [+] adding a function to detect function with index.

---
+++ src/utils/has_idx.c
@@ -0,0 +1,26 @@
+/*
+** EPITECH PROJECT, 2024
+** corewar-dev
+** File description:
+** has_idx.c
+*/
+
+int has_idx(unsigned char code, int ac)
+{
+    switch (code) {
+        case 9:
+            return 1;
+        case 10:
+            return (ac < 3);
+        case 14:
+            return (ac < 3);
+        case 11:
+            return (ac > 1);
+        case 12:
+            return 1;
+        case 15:
+            return 1;
+        default:
+            return 0;
+    }
+}
