17325532cb68c23f5d2730331a6daf910e6a18ed
Rafael Drouart <rafael.drouart@epitech.eu>
2024-05-15 14:08:59 +0200

    [~] Modifying the function that get the -n value to get the id of each champion

--- src/error/check_champions.c
+++ src/error/check_champions.c
@@ -17,22 +17,23 @@
 #include "corewar.h"
 #include "my.h"
 
-int set_champion_id(champion_t *champion, char const *id_str)
+int set_champion_id(corewar_t *corewar, char **av, int i, int j)
 {
-    if (my_str_isnum(id_str) == 0) {
-        champion->id = my_getnbr(id_str);
+    if (my_str_isnum(av[i + 1]) == 0) {
+        corewar->n_value[j] = my_atoi(av[i + 1]);
+        corewar->champ_names[j] = av[i + 2];
         return 0;
     } else {
         return 84;
     }
 }
 
-int check_options_n(char **av, int i, corewar_t *corewar)
+int check_options_n(char **av, int *i, corewar_t *corewar, int j)
 {
-    if (av[i + 1] == NULL)
+    if (av[(*i) + 1] == NULL)
         return 84;
-    if (set_champion_id(corewar->champs, av[i + 1]) == 0)
-        i++;
+    if (set_champion_id(corewar, av, (*i), j) == 0)
+        (*i)++;
     else
         return 84;
     return 0;
@@ -42,14 +43,19 @@ int check_champion(corewar_t *corewar, char **av)
 {
     int i = 1;
     int result = 0;
+    static int j = 0;
 
     for (; av[i]; i++) {
         if (result == 84)
             return 84;
         if (my_strcmp(av[i], "-n") == 0) {
-            result = check_options_n(av, i, corewar);
+            result = check_options_n(av, &i, corewar, j);
+            i++;
+            j++;
             continue;
         }
+        if (get_extension(av[i]) == 0)
+            j++;
     }
     return 0;
 }
