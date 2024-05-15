3b271a7ac76263859c13d003904674aeb91d08e0
Rafael Drouart <rafael.drouart@epitech.eu>
2024-05-15 10:23:14 +0200

    [~] Fixing the check_args function to correctly get the .cor in the arguments

--- src/error/check_args.c
+++ src/error/check_args.c
@@ -24,13 +24,13 @@ int check_args(char **av, corewar_t *corewar)
     int j = 0;
 
     for (int i = 1; av[i] && champions_loaded < 4; i++)
-        if (!get_extension(av[i]) == 0)
+        if (get_extension(av[i]) == 0)
             champions_loaded++;
     corewar->fd = malloc(champions_loaded * sizeof(int));
     if (corewar->fd == NULL)
         return -1;
     for (int i = 1; av[i] && j < champions_loaded; i++) {
-        if (!get_extension(av[i]) == 0) {
+        if (get_extension(av[i]) == 0) {
             corewar->fd[j] = open(av[i], O_RDONLY);
             j++;
         }
