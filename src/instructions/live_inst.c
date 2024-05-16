a82351303c434e3be69bb85038d137965eaafcf8
Mathis Bukowski <mathis.bukowski@epitech.eu>
2024-05-16 21:46:45 +0200

    [+] adding live command

---
+++ src/instructions/live_inst.c
@@ -0,0 +1,13 @@
+/*
+** EPITECH PROJECT, 2024
+** corewar-dev
+** File description:
+** live_instr.c
+*/
+
+#include "corewar.h"
+
+int live_command(instruction_t *instr, champion_t *champ, corewar_t *corewar)
+{
+
+}
