24d251aeacc2f3f9a58ab6d7de0f68e671409f28
Mathis Bukowski <mathis.bukowski@epitech.eu>
2024-05-16 13:56:39 +0200

    [~] editing params in this function

--- src/utils/has_idx.c
+++ src/utils/has_idx.c
@@ -5,19 +5,17 @@
 ** has_idx.c
 */
 
-int has_idx(unsigned char code, int ac)
+int has_idx(int code, int ac)
 {
     switch (code) {
         case 9:
             return 1;
         case 10:
-            return (ac < 3);
         case 14:
             return (ac < 3);
         case 11:
             return (ac > 1);
         case 12:
-            return 1;
         case 15:
             return 1;
         default:
