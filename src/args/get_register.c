be7a374c723b508a2dc8189b654cf60757a9aff7
Mathis Bukowski <mathis.bukowski@epitech.eu>
2024-05-16 13:52:45 +0200

    [+] adding decode register function

--- src/args/get_register.c
+++ src/args/get_register.c
@@ -11,6 +11,5 @@ int get_register_argument(corewar_t *corewar, champion_t *champ)
 {
     int arg = (int)corewar->arena->memory[(champ->pc + 2) % MEM_SIZE];
 
-    //champ->pc += 1;
     return arg;
 }
